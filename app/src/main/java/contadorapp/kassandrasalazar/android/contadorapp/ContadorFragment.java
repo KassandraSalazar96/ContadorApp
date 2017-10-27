792ee12c79428c159de051954f7e16d2234f29a7
Kassandra Salazar <14470225@itcampeche.edu.mx>
2017-10-27 01:27:15 -0500

    Segunda version del proyecto ContadorApp.No incluye manejo de eventos.

---
+++ app/src/main/java/contadorapp/kassandrasalazar/android/contadorapp/ContadorFragment.java
@@ -0,0 +1,26 @@
+package contadorapp.kassandrasalazar.android.contadorapp;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by KASSANDRA-PC on 27/10/2017.
+ */
+
+public class ContadorFragment extends Fragment {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragmento_contador, container, false);
+        return v;
+    }
+}
--- app/src/main/java/contadorapp/kassandrasalazar/android/contadorapp/MainActivity.java
+++ app/src/main/java/contadorapp/kassandrasalazar/android/contadorapp/MainActivity.java
@@ -1,13 +1,24 @@
 package contadorapp.kassandrasalazar.android.contadorapp;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends FragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.Fragment_container);
+        if (fragment == null) {
+            fragment = new ContadorFragment();
+            fm.beginTransaction()
+                    .add(R.id.Fragment_container, fragment)
+                    .commit();
+        }
     }
 }
